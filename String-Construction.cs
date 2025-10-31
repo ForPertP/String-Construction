80e772709f7689e41c571381c5c9df3ef70f0713
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-01 05:54:44 +0900

    Implement skeleton for stringConstruction function
    Added the skeleton for the stringConstruction function.

--- String-Construction.cs
+++ String-Construction.cs
@@ -11,3 +11,21 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+
+class Result
+{
+
+    /*
+     * Complete the 'stringConstruction' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int stringConstruction(string s)
+    {
+
+    }
+
+}
