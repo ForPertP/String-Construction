fe51edf62ba4ea55f2f2844443a56023409c724f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-10 09:42:14 +0900

    Update String-Construction.java

--- String-Construction.java
+++ String-Construction.java
@@ -10,6 +10,14 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+class Result {
+
+    /*
+     * Complete the 'stringConstruction' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
 
     public static int stringConstruction(String s) {
         HashSet<Character> uniqueChars = new HashSet<>();
@@ -22,7 +30,6 @@ import static java.util.stream.Collectors.toList;
     }    
 
 
-
     public static int stringConstruction2(String s) {
         TreeSet<Character> uniqueChars = new TreeSet<>();
 
@@ -32,7 +39,7 @@ import static java.util.stream.Collectors.toList;
 
         return uniqueChars.size();
     }
-
+}
 
 
 public class Solution {
